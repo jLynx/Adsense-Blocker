48756835b32b8f160ce2d6eb05d517fb725badcb
DarkN3ss <karter61@gmail.com>
2014-11-26 14:01:17 +1300

    Create adBlock.js

---
+++ AdsenseBlocker/adBlock.js
@@ -0,0 +1,29 @@
+var isOverGoogleAd = false;
+
+var ad = /adsbygoogle/;
+
+$(document).ready(function()
+{	
+	$('ins').live('mouseover', function () {
+		if(ad.test($(this).attr('class'))){
+			isOverGoogleAd = true;
+		}
+	});
+	$('ins').live('mouseout', function () {
+		if(ad.test($(this).attr('class'))){
+			isOverGoogleAd = false;
+		}
+	});
+});
+
+$(window).blur(function(e){
+	if(isOverGoogleAd){
+		$.ajax({
+			type: "post",
+			url: "adblocker/blocker.php",
+			data: {
+				adUrl: window.location.href
+				}
+		});
+	}
+});
